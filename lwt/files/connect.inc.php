b0ee353150701c12d80f5736249bcd9d58605f49
Krisoijn Chan <krisoijn.chan@gmail.com>
2013-04-21 12:35:10 +1000

    add git n lwt

---
+++ lwt/files/connect.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+/**************************************************************
+"Learning with Texts" (LWT) is released into the Public Domain.
+This applies worldwide.
+In case this is not legally possible, any entity is granted the
+right to use this work for any purpose, without any conditions, 
+unless such conditions are required by law.
+
+
+Developed by J. P. in 2011, 2012, 2013.
+***************************************************************/
+
+/**************************************************************
+DB variables for XAMPP
+***************************************************************/
+
+$server = "localhost";
+$userid = "lwt";
+$passwd = "{{mysql_lwt_password}}";
+$dbname = "lwt";
+
+?>
